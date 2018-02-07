8e4b1b3b073a946f6b18179fd6e81cda99efaf50
Rick <rdsouthard1979@gmail.com>
2018-02-07 07:21:39 -0600

    JS works.  Added CSS file.

--- wiki.js
+++ wiki.js
@@ -1,39 +1,35 @@
-var apiURL = "https://en.wikipedia.org/w/api.php?callback=?";
+function fetch(query) {
+  return $.ajax({
+    url: 'http://en.wikipedia.org/w/api.php',
+    data: { action: 'query',
+            list: 'search',
+            srsearch: query,
+            format: 'json' },
+    dataType: 'jsonp',
 
-$(document).ready(function() {
-  $('#button').click(function() {
-    $("#result").empty(); // clear prior search results
-    $.getJSON(apiURL, {
-        action: 'query',
-        format: 'json',
-        inprop: "url",
-        formatversion: 2,
-        generator: 'search',
-        gsrsearch: $("wiki").val(),
-        gsrwhat: "text",
-        prop: 'extracts|info',
-        exsentences: 3,
-        exintro: "",
-        explaintext: "",
-        exlimit: 20,
-      })
-      .success(function(response) {
-        //console.log(response);
-        response.query.pages.forEach(function(resp) {
-          $('#results').append(
-            "<a href='" + resp.fullurl + "' target= '_blank'><div id='result'><h3>" + resp.title + "</h3><p = class='extract'>" + resp.extract + "</p></div>");
-        })
-      });
-  }); // search
-
-  //$('#button').click(function() {
-
-  //}); // randomPage
-
-  // trigger submit on use of enter key
-  $("#wiki").keyup(function(event) {
-    if (event.keyCode == 13) {
-      $("#button").click();
-    }
   });
+  console.log(query);
+}
+
+function render(results) {
+  var $out = $("#results");
+  var html = results.map(function(result) {
+    return '<a class="result"' +
+           '   href="https://en.wikipedia.org/wiki/'+result.title+'">' +
+           '  <h2 class="text-center">'+result.title+'</h2>' +
+           '  <div class="text-center">'+result.snippet+'</div>' +
+           '</a>';
+  }).join("\n\n");
+  $out.html("");
+  $(html).appendTo($out);
+}
+
+$(".search-form").on("submit", function(event) {
+  event.preventDefault();
+  var query = $("#wiki").val();
+  fetch(query)
+    .done(function(data) {
+      console.log(data);
+      render(data.query.search);
+    });
 });
